2730101c91d91050236d716de14580dd047f5b7a
amuarte <adamczykkrystian28@gmail.com>
2025-12-11 14:57:42 +0100

    Add project files.

---
+++ KogamaStudio/DllLoader.cs
@@ -0,0 +1,18 @@
+﻿using MelonLoader;
+using System.Runtime.InteropServices;
+
+namespace KogamaStudio
+{
+    internal class DllLoader
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string dllToLoad);
+
+        public static void Load(string dllName)
+        {
+            IntPtr handle = LoadLibrary(dllName);
+            if (handle == IntPtr.Zero)
+                MelonLogger.Error($"Failed to load {dllName}");
+        }
+    }
+}
---
+++ KogamaStudio/TextureSwapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+
+namespace KogamaStudio
+{
+    internal class TextureSwapper
+    {
+        public static void SwapFireTexture(string customTexturePath)
+        {
+            try
+            {
+                byte[] imageData = File.ReadAllBytes(customTexturePath);
+                Texture2D newTexture = new Texture2D(2, 2);
+                newTexture.LoadImage(imageData);
+
+                MelonLogger.Msg("Fire texture loaded!");
+                
+            } 
+            catch (Exception ex)
+            {
+                MelonLogger.Msg($"Failed: {ex.Message}");
+            }
+
+        }
+    }
+}
