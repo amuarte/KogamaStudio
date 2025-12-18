79a328562fab12b38eeaaecae25c9ad6cd297c51
amuarte <adamczykkrystian28@gmail.com>
2025-12-18 21:16:40 +0100

    anti-ban added

--- KogamaStudio/AntiBan.cs
+++ KogamaStudio/AntiBan.cs
@@ -1,24 +1,29 @@
-﻿//using HarmonyLib;
-//using Il2Cpp;
-//using MV.Common;
+﻿using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+using MV.Common;
+using System.Diagnostics;
 
-//namespace KogamaStudio;
+namespace KogamaStudio;
 
-//[HarmonyPatch]
-//internal static class AntiBan
-//{
-//    // tysm beckowl
+[HarmonyPatch]
+internal static class AntiBan
+{
+    // tysm beckowl
 
-//    [HarmonyPatch(typeof(CheatHandling), "Init")]
-//    [HarmonyPatch(typeof(CheatHandling), "ExecuteBan")]
-//    [HarmonyPatch(typeof(CheatHandling), "MachineBanDetected")]
-//    [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Ban", [typeof(int), typeof(MVPlayer), typeof(string)])]
-//    //[HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Ban", [typeof(CheatType)])]
-//    [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Expel")]
-//    [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Kick")]
-//    [HarmonyPrefix]
-//    private static bool NoBan()
-//    {
-//        return false;
-//    }
-//}
+    [HarmonyPatch(typeof(CheatHandling), "Init")]
+    [HarmonyPatch(typeof(CheatHandling), "ExecuteBan")]
+    [HarmonyPatch(typeof(CheatHandling), "CheatSoftwareRunningDetected")]
+    [HarmonyPatch(typeof(CheatHandling), "TextureHackDetected")]
+    [HarmonyPatch(typeof(CheatHandling), "MachineBanDetected")]
+    [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Ban", [typeof(int), typeof(MVPlayer), typeof(string)])]
+    //[HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Ban", [typeof(CheatType)])]
+    [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Expel")]
+    [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Kick")]
+    [HarmonyPrefix]
+    private static bool NoBan()
+    {
+        return false;
+    }
+
+}
